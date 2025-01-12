f8d1992d0138f1cd573192ab008fbf8edceedc65
Nicola <nicolatournay@outlook.com>
2025-01-12 22:27:35 +0100

    Add starter codebase to Git.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
